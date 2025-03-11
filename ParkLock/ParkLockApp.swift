10af7a5933c20f6b48319acacd9b3428e0555683
Matthew Collins <matthewcollins@Matthews-MacBook-Pro.local>
2025-03-11 02:05:24 -0400

    foundational commit

---
+++ ParkLock/ParkLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkLockApp.swift
+//  ParkLock
+//
+//  Created by Matthew Collins on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ParkLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ParkLockTests/ParkLockTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParkLockTests.swift
+//  ParkLockTests
+//
+//  Created by Matthew Collins on 3/11/25.
+//
+
+import Testing
+@testable import ParkLock
+
+struct ParkLockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
